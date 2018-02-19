08f32e2b475a4f52d51b3b6b3cf25b2ce9bbaff5
kujiraitakahiro <takahiro.kujirai@it.zeus-enterprise.co.jp>
2018-02-19 16:53:23 +0900

    typo for case.sh

--- case.sh
+++ case.sh
@@ -5,5 +5,5 @@ case $1 in
  2)
   echo "Very Nice! Challenge LinuC Level 3.";;
  3)
-  echo "Excelent! You are Speciality LinuC engeneering!!";;
+  echo "Excellent! You are Speciality LinuC engeneering!!";;
 esac
